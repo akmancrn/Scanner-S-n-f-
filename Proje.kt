8a3f08a44cd26c7c962942e1ccc254db5b669c28
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 12:44:11 +0300

    Create Proje.kt

---
+++ Proje.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+
+import java.io.PrintStream
+import java.util.Scanner
+import java.util.*
+
+fun main (args : Array<String>)
+{
+   var veri = Scanner(System.`in`)
+   print("Metin giriniz")
+   var txt1 = veri.next();
+   print("next() ile alınan metin: $txt1'dir")
+
+
+}
